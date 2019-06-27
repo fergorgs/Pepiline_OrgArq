92f56791585fa643bbce8d9ad1919de8d9067b3d
fergorgs <fer.fayet@usp.br>
2019-06-27 15:56:04 -0300

    Sprites Regs intermediarios + Bugs

---
+++ Pipeline/Assets/RegsLabelScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegsLabelScript : MonoBehaviour
+{
+	private GameObject father;
+
+	private TextMesh text;
+
+	private double r, g, b;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		father = transform.parent.gameObject;
+
+		text = GetComponent<TextMesh>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		r = 0.5 - (father.GetComponent<SpriteRenderer>().color.r - 0.5);
+		g = 0.5 - (father.GetComponent<SpriteRenderer>().color.g - 0.5);
+		b = 0.5 - (father.GetComponent<SpriteRenderer>().color.b - 0.5);
+
+		//text.color = new Color((float)r, (float)g, (float)b);
+    }
+}
